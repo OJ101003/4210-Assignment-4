013d7d2c0ad8eb8337d52092000e5c2297deab29
OJ101003 <hackerdude1010@gmail.com>
2025-04-27 16:02:33 -0700

    cnn py complete

--- cnn.py
+++ cnn.py
@@ -1,9 +1,8 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Omar Jaber
+# FILENAME: cnn.py
+# SPECIFICATION: CNN that uses tensorflow and keras to recognize hand-written digits
 # FOR: CS 4210 - Assignment #4
-# TIME SPENT: how long it took you to complete the assignment
 #-------------------------------------------------------------------------
 
 # Importing Python libraries
@@ -11,7 +10,7 @@ import os
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from keras import layers, models
 
 # Function to load dataset
 def load_digit_images_from_folder(folder_path, image_size=(32, 32)):
@@ -20,7 +19,7 @@ def load_digit_images_from_folder(folder_path, image_size=(32, 32)):
     for filename in os.listdir(folder_path):
         # Getting the label of the image (it's the first number in the filename)
         # --> add your Python code here.
-        label = ?
+        label = filename.split('_')[0]
 
         # Converting images to grayscale and resizing them to 32x32
         img = Image.open(os.path.join(folder_path, filename)).convert('L').resize(image_size)
@@ -28,7 +27,8 @@ def load_digit_images_from_folder(folder_path, image_size=(32, 32)):
         # Adding the converted image to the feature matrix and label to the class vector
         X.append(np.array(img))
         y.append(label)
-    return np.array(X), np.array(y)
+    return np.array(X).astype('int64'), np.array(y).astype('int64')
+
 
 # Set your own paths here (relative to your project folder)
 train_path = os.path.join("images", "train")
@@ -37,18 +37,18 @@ test_path = os.path.join("images", "test")
 # Loading the raw images using the provided function. Hint: Use the provided load_digit_images_from_folder function that outputs X_train, Y_train for train_path and
 # as X_test, Y_test for test_path
 # --> add your Python code here
-X_train, Y_train = ?
-X_test, Y_test = ?
+X_train, Y_train = load_digit_images_from_folder("test")
+X_test, Y_test = load_digit_images_from_folder("train")
 
 # Normalizing the data: convert pixel values from range [0, 255] to [0, 1]. Hint: divide them by 255
 # --> add your Python code here
-X_train = ?
-X_test = ?
+X_train = X_train/255.0
+X_test = X_test/255.0
 
 # Reshaping the input images to include the channel dimension: (num_images, height, width, channels)
 # --> add your Python code here
-X_train = X_train.reshape(?)
-X_test = X_test.reshape(?)
+X_train = np.expand_dims(X_train, axis=-1)
+X_test = np.expand_dims(X_test, axis=-1)
 
 # Building a CNN model
 model = models.Sequential([
@@ -56,34 +56,40 @@ model = models.Sequential([
     # Add a convolutional layer with 32 filters of size 3x3, relu activation, and input shape 32x32x1
     # Use layers.[add a layer here],
     # --> add your Python code here
+    layers.Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 1), activation='relu'),
 
     # Add a max pooling layer with pool size 2x2
     # Use layers.[add a layer here],
     # --> add your Python code here
+    layers.MaxPooling2D(pool_size=(2,2)),
 
     # Add a flatten layer to convert the feature maps into a 1D vector
     # Use layers.[add a layer here],
     # --> add your Python code here
+    layers.Flatten(),
 
     # Add a dense (fully connected) layer with 64 neurons and relu activation
     # Use layers.[add a layer here],
     # --> add your Python code here
+    layers.Dense(64, activation='relu'),
 
     # Add the output layer with 10 neurons (digits 0–9) and softmax activation
     # Use layers.[add a layer here]
     # --> add your Python code here
+    layers.Dense(10, activation='softmax')
 ])
+
 
 # Compiling the model using optimizer = sgd, loss = sparse_categorical_crossentropy, and metric = accuracy
 # --> add your Python code here
-model.compile(optimizer=?, loss=?, metrics=[?])
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Fitting the model with batch_size=32 and epochs=10
 # --> add your Python code here
-model.fit(X_train, Y_train, batch_size=?, epochs=?, validation_data=(X_test, Y_test))
-
+model.fit(X_train, Y_train, batch_size=32, epochs=10, validation_data=(X_test, Y_test))
 # Evaluating the model on the test set
 loss, acc = model.evaluate(X_test, Y_test)
 
 # Printing the test accuracy
 # --> add your Python code here
+print(f"Test accuracy: {acc}\nTest loss: {loss}")
